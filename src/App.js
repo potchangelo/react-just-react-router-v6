277c5d317f9928604871cafb47d29130a5f29e9b
Potchara Puttawanchai <potchangelo@gmail.com>
2022-02-16 15:06:42 +0700

    React Router v.6 routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import { Route, Switch } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import AppHeader from './AppHeader';
+import PageAbout from './PageAbout';
+import PageHome from './PageHome';
 
 function App() {
   return (
     <div className="App">
       <AppHeader />
+      <Routes>
+        <Route path="/" exact element={<PageHome />} />
+        <Route path="/about" element={<PageAbout />} />
+      </Routes>
     </div>
   );
 }
