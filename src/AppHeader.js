467cdc53112bf95df582188f16cef961637a2cd9
Potchara Puttawanchai <potchangelo@gmail.com>
2022-02-16 14:53:17 +0700

    React Router v.6 NavLink

--- src/AppHeader.js
+++ src/AppHeader.js
@@ -2,10 +2,16 @@ import { NavLink } from 'react-router-dom';
 import './AppHeader.css';
 
 function AppHeader() {
+  function getNavClass(navLinkProps) {
+    let navClass = 'app-header-item';
+    if (navLinkProps.isActive) navClass += ' app-header-item-active';
+    return navClass;
+  }
+
   return (
     <header className="app-header">
-      <NavLink className="app-header-item" to="/" exact>Homepage</NavLink>
-      <NavLink className="app-header-item" to="/about">About</NavLink>
+      <NavLink className={getNavClass} to="/" exact>Homepage</NavLink>
+      <NavLink className={getNavClass} to="/about" exact>About</NavLink>
     </header>
   );
 }
