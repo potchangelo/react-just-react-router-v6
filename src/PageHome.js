c2bf3d3c91bbc2df60b3d09b1b04d91da369005a
Potchara Puttawanchai <potchangelo@gmail.com>
2023-07-01 18:06:27 +0700

    Prettier format

--- src/PageHome.js
+++ src/PageHome.js
@@ -1,6 +1,6 @@
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import LayoutPage from "./LayoutPage";
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import LayoutPage from './LayoutPage';
 
 function PageHome() {
   const [postArray, setPostArray] = useState([]);
